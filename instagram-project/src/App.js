6f2a718b65f26497c1ca5dc14ff6d536837f0c41
James Troughton <jamesstroughton@gmail.com>
2020-09-04 18:59:27 +1200

    second commit

--- instagram-project/src/App.js
+++ instagram-project/src/App.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Post from './Post';
+import { db } from './firebase';
+
 
 function App() {
-  return (
-    <div className="App">
-      
-      {/* Header */}
+  const [posts, setPosts] = useState([]);
 
-      {/* Posts */}
-      {/* Posts */}
+  useEffect(() => {
+    db.collection('posts').onSnapshot(snapshot => {
+      setPosts(snapshot.docs.map(doc => doc.data()))
+    })
+  }, []);
+
+  return (
+    <div className="app">
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
+        />
+      </div>
+
+      <h1>Posts</h1>
+
+      {
+        posts.map(post => (
+          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
+        ))
+      }
+
 
     </div>
   );
